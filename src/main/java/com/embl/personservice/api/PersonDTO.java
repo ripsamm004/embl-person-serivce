d9c34d97c436ed7a71de7588f7701387c6ae8cf9
shiful <rikto.islam@gmail.com>
2018-06-02 15:58:01 +0100

    clean code

--- src/main/java/com/embl/personservice/api/PersonDTO.java
+++ src/main/java/com/embl/personservice/api/PersonDTO.java
@@ -3,14 +3,13 @@ package com.embl.personservice.api;
 
 import com.embl.personservice.domain.Person;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @AllArgsConstructor
-@Getter
 @ToString
 public class PersonDTO {
     private final List<Person> person;
@@ -18,5 +17,9 @@ public class PersonDTO {
     public PersonDTO(Person persons){
         person= new ArrayList<>();
         this.person.add(persons);
+    }
+
+    public List<Person> getPerson() {
+        return Collections.unmodifiableList(person);
     }
 }
